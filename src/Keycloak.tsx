6add9b2a71dc4c4b367a6d853c1e80b3edc9c3bf
Auleksis <auleks.red@gmail.com>
2024-11-05 20:24:09 +0300

    keycloak bug fixed

--- src/Keycloak.tsx
+++ src/Keycloak.tsx
@@ -1,7 +1,7 @@
 import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
-  url: import.meta.env.VITE_REACT_APP_KEYCLOAK_ENDPOINT,
+  url: import.meta.env.VITE_REACT_APP_KEYCLOAK_NEW_ENDPOINT,
   realm: "running-community",
   clientId: "React-auth",
 });
